2993023a6f1a55bfbe748ce235c0b3d24938fbc0
Zhangzqs <2428698039@qq.com>
2020-08-03 16:23:25 +0800

    测试类

---
+++ test/JsPlayTest.java
@@ -0,0 +1,10 @@
+import cn.zzq.aix.builder.AIXBuilder;
+import cn.zzq.aix.builder.utils.Path;
+
+public class JsPlayTest {
+	public static void main(String[] args) {
+		Path pj = new Path().backward().forward("AIX-JsPlay");
+		AIXBuilder.main(new String[] { pj.toString() });
+		Color
+	}
+}
